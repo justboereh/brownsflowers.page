84957b8d834b5218e835bd911e7f08dc4a6ab946
justboereh <justboereh@gmail.com>
2023-04-04 10:02:24 -0500

    type fix

--- functions/api/flowers/[slug].ts
+++ functions/api/flowers/[slug].ts
@@ -5,7 +5,8 @@ interface Env {
 }
 
 export const onRequestGet: PagesFunction = ({ params }) => {
-    if (!Flowers.has(params.slug)) return new Response('Invalid', { status: 404 })
+    if (!Flowers.has(params.slug as string))
+        return new Response('Invalid', { status: 404 })
 
-    return new Response(JSON.stringify(Flowers.get(params.slug)))
+    return new Response(JSON.stringify(Flowers.get(params.slug as string)))
 }
