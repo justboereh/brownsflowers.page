4e4adbfa93d0a22373124fca9782ffe42c4f0f5f
justboereh <justboereh@gmail.com>
2023-04-04 20:49:34 -0500

    removed mysql2 connection

--- server/middleware/database.ts
+++ server/middleware/database.ts
@@ -1,9 +1,8 @@
-import { createConnection } from 'mysql2'
+// import { createConnection } from 'mysql2'
 import { Deta } from "deta";
 
-const connection = createConnection(process.env.DATABASE_URL)
+// const connection = createConnection(process.env.DATABASE_URL)
 
-export default defineEventHandler((event) => {
-    event.context.connection = connection
-    event.context.deta = Deta(process.env.DETA_PROJECT_KEY)
+ export default defineEventHandler((event) => {
+   event.context.deta = Deta(process.env.DETA_PROJECT_KEY)
 })
